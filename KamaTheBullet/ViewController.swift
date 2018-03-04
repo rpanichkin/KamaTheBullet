41aaef47982262b5ef166a605847515cc3ea0164
osafin <osafin@crystalnix.com>
2018-03-04 16:24:04 +0600

    falling, talking and cool staff :D

--- KamaTheBullet/ViewController.swift
+++ KamaTheBullet/ViewController.swift
@@ -9,8 +9,15 @@
 import UIKit
 import SceneKit
 import ARKit
+import AVFoundation
 
-class ViewController: UIViewController, ARSCNViewDelegate {
+class ViewController: UIViewController, ARSCNViewDelegate, SCNNodeRendererDelegate {
+    @IBOutlet weak var wastedLabel: UILabel!
+    
+    var timer: Timer?
+    private static var myContext = 0
+    
+    var player: AVAudioPlayer?
     
     @IBOutlet var sceneView: ARSCNView!
     
@@ -18,11 +25,18 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     
     var surfaceDictionary: [UUID: SCNNode] = [:]
     
+    var kamaExists = false
+    
+    var kamaOnScene: SCNNode?
+    var kamaItitialPosition: SCNVector3?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Set the view's delegate
         sceneView.delegate = self
+        
+        
         
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
@@ -46,12 +60,45 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.run(configuration)
         
         sceneView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(insetCubeRecognizer)))
+        
     }
     
+    @IBAction func eraseButtonAction(_ sender: Any) {
+        if !kamaExists {
+            return
+        }
+        
+        kamaOnScene?.removeFromParentNode()
+        kamaOnScene = nil
+        kamaExists = false
+        
+        wastedLabel.alpha = 0
+        
+        timer?.invalidate()
+        timer = nil
+    }
+    
+    
     @objc func insetCubeRecognizer(gesture: UIGestureRecognizer) {
-        // Take the screen space tap coordinates and pass them to the hitTest method on the ARSCNView instance
+        
+                // Take the screen space tap coordinates and pass them to the hitTest method on the ARSCNView instance
         let tapPoint = gesture.location(in: sceneView)
         let result = sceneView.hitTest(tapPoint, types: .existingPlaneUsingExtent)
+        
+        if kamaExists {
+            let hits = sceneView.hitTest(tapPoint, options: nil)
+            if let node = hits.first?.node {
+
+                if node == kamaOnScene {
+                    handleKamaTap()
+                }
+
+                return
+            }
+
+            return
+        }
+
         
         // If the intersection ray passes through any plane geometry they will be returned, with the planes
         // ordered by distance from the camera
@@ -63,18 +110,32 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         if (firstHit == nil) {
             firstHit = result[0]
         } else {
-            //            insertCube(hitResult: result[0])
-            
             insertKama(secondHit: result[0])
             firstHit = nil
+            
+            kamaExists = true
         }
+    }
+    
+    func handleKamaTap() {
+        let phrases = [
+        "shaa", "diavol", "sport", "protivnik"
+        ]
+        
+        let count = UInt32(phrases.count)
+        guard count > 0 else { return  }
+        
+        let idx = Int(arc4random_uniform(count))
+        
+        let phrase = phrases[idx]
+        
+        Thread.detachNewThreadSelector(#selector(playSound(assetName:)), toTarget: self, with: phrase)
     }
     
     func insertKama(secondHit: ARHitTestResult) {
         guard let firstHit = firstHit else {
             return
         }
-        
         
         
         guard let firstSurface = firstHit.anchor as? ARPlaneAnchor else {
@@ -84,14 +145,15 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             return
         }
         
-        print(firstSurface.center.y)
-        print(secondSurface.center.y)
         let maxY = max(firstSurface.transform.columns.3.y, secondSurface.transform.columns.3.y)
-        print(maxY)
         
         let center = centerBetweenHits(firstHit, secondHit)
         
-        let kamaCube = SCNBox(width: 1, height: 0.5, length: 0.1, chamferRadius: 0)
+        let width = max(CGFloat(distanceBetweenHit(firstHit, secondHit)), 0.3)
+        let kamaCube = SCNBox(width: width,
+                              height: 0.5 * width,
+                              length: 0.1 * width,
+                              chamferRadius: 0)
         let kamaMaterial = SCNMaterial()
         kamaMaterial.diffuse.contents = #imageLiteral(resourceName: "kama")
         let kamaFlippedMaterial = SCNMaterial()
@@ -104,8 +166,6 @@ class ViewController: UIViewController, ARSCNViewDelegate {
                                   maxY + 0.1,
                                   center.1)
         
-        //        let cube = SCNBox(width: 0.25, height: 0.1, length: 0.05, chamferRadius: 0)
-        
         let kamaNode = SCNNode(geometry: kamaCube)
         kamaNode.physicsBody = SCNPhysicsBody(type: .dynamic, shape: nil)
         
@@ -114,7 +174,33 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         kamaNode.position = position
         
+        kamaNode.runAction(SCNAction.rotateBy(x: 0,
+                                              y: CGFloat(angleForTouchPoints(firstHit, secondHit) - Float(Double.pi / 2)),
+                                              z: 0,
+                                              duration: 0))
+        
+        kamaNode.rendererDelegate = self
+        
         sceneView.scene.rootNode.addChildNode(kamaNode)
+        
+        kamaOnScene = kamaNode
+        
+        perform(#selector(startCheckingKamaPosition), with: nil, afterDelay: 0.5)
+        
+        Thread.detachNewThreadSelector(#selector(playSound(assetName:)), toTarget: self, with: "salam")
+    }
+    
+    func angleForTouchPoints(_ first: ARHitTestResult, _ second: ARHitTestResult) -> Float {
+        let x1 = first.worldTransform.columns.3.x
+        let x2 = second.worldTransform.columns.3.x
+        
+        let y1 = first.worldTransform.columns.3.z
+        let y2 = second.worldTransform.columns.3.z
+        
+        let deltaX = x2 - x1
+        let deltaY = y2 - y1
+        
+        return atan2(deltaX, deltaY)
     }
     
     func centerBetweenHits(_ first: ARHitTestResult, _ second: ARHitTestResult) -> (Float, Float) {
@@ -130,12 +216,73 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         return (x3, y3)
     }
     
-    func insertCube(hitResult: ARHitTestResult) {
+    func distanceBetweenHit(_ first: ARHitTestResult, _ second: ARHitTestResult) -> Float {
+        let x1 = first.worldTransform.columns.3.x
+        let x2 = second.worldTransform.columns.3.x
         
+        let y1 = first.worldTransform.columns.3.z
+        let y2 = second.worldTransform.columns.3.z
         
+        return sqrt(pow((x2 - x1), 2) + pow((y2 - y1), 2))
+    }
+    
+    @objc func playSound(assetName: String) {
+        if player?.isPlaying ?? false {
+            return
+        }
         
+        guard let url = Bundle.main.url(forResource: assetName, withExtension: "mp3") else {
+            return
+        }
         
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            guard let player = player else { return }
+            
+            player.prepareToPlay()
+            player.play()
+            
+        } catch let error as NSError {
+            print(error.description)
+        }
+    }
+    
+    @objc func startCheckingKamaPosition() {
+        kamaItitialPosition = kamaOnScene?.presentation.position
+        if timer == nil {
+            timer = Timer.scheduledTimer(timeInterval: 0.1,
+                                         target: self,
+                                         selector: #selector(checkKama),
+                                         userInfo: nil,
+                                         repeats: true)
+        }
+    }
+    
+    @objc func checkKama() {
+        guard let currentKama = kamaOnScene?.presentation, let initialPosition = kamaItitialPosition else {
+            fatalError("no Kama to check")
+        }
         
+        if initialPosition.x.rounded(toPlaces: 2) != currentKama.position.x.rounded(toPlaces: 2)
+            || initialPosition.y.rounded(toPlaces: 2) != currentKama.position.y.rounded(toPlaces: 2)
+            || initialPosition.z.rounded(toPlaces: 2) != currentKama.position.z.rounded(toPlaces: 2) {
+            kamaFalls()
+            timer?.invalidate()
+            timer = nil
+        }
+        
+    }
+    
+    func kamaFalls() {
+        wastedLabel.alpha = 1
+        
+        perform(#selector(removeWastedLabel), with: nil, afterDelay: 2)
+    }
+    
+    @objc func removeWastedLabel()  {
+        UIView.animate(withDuration: 2) {
+            self.wastedLabel.alpha = 0
+        }
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -153,6 +300,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     // MARK: - ARSCNViewDelegate
     
     func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+
+        
         guard let planeAnchor = anchor as? ARPlaneAnchor else {
             return
         }
@@ -198,6 +347,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
     }
     
+    
+    
     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
         let node = SCNNode()
         
@@ -222,3 +373,11 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     }
 }
 
+
+extension Float {
+    /// Rounds the double to decimal places value
+    func rounded(toPlaces places:Int) -> Float {
+        let divisor = pow(10.0, Float(places))
+        return (self * divisor).rounded() / divisor
+    }
+}
